4b7e3ac364c58e893d0e64b54c3fc6d3c45cbd5d
Colin Frankish <colin.frankish@gmail.com>
2014-02-10 22:52:27 +0000

    database cleaner added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,21 @@
 ENV["RACK_ENV"] = 'test'
 
 require './server'
+require 'database_cleaner'
 
 Rspec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.order = 'random'
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
