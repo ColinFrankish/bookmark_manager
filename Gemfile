e3a24d210de2b93f65cfabae609eda69acea3df1
Colin Frankish <colin.frankish@gmail.com>
2014-02-10 18:57:40 +0000

    Rspec added and error fixed with Postgress

--- Gemfile
+++ Gemfile
@@ -6,3 +6,6 @@ gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 
+group :development, :test do
+  gem "rspec"
+end
--- server.rb
+++ server.rb
@@ -3,3 +3,13 @@ require '/sinatra'
 get '/' do
   erb :index
 end
+
+env = ENV["RACK_ENV"] || "development"
+
+DataMapper.setup(default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link'
+
+DataMapper.finalize
+DataMapper.Auto_Upgrade!
+
