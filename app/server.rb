748a2923177eff7971ba8117969977b014ce68e4
Colin Frankish <colin.frankish@gmail.com>
2014-02-12 14:03:25 +0000

    Users checked for uniquness - 1 error

--- app/server.rb
+++ app/server.rb
@@ -44,11 +44,11 @@ post '/users' do
               :password => params[:password], 
               :password_confirmation => params[:password_confirmation])
   if @user.save
-  session[:user_id] = @user.id
-  redirect to('/')
-  else
-    flash[:notice] = "Sorry, your passwords don't match."
-  erb :"users/new"
+    session[:user_id] = @user.id
+    redirect to('/')
+    else
+    flash.now[:errors] = @user.errors.full_messages
+    erb :"users/new"
   end  
 end
 
