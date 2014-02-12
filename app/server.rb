4afe6b4ce5e9419b5ec43357de7cc6cc87d8174c
Colin Frankish <colin.frankish@gmail.com>
2014-02-12 16:23:20 +0000

    User sign in added and bugfixed

--- app/server.rb
+++ app/server.rb
@@ -39,6 +39,24 @@ get '/users/new' do
   erb :"users/new"
 end
 
+get '/sessions/new' do
+  erb :"sessions/new"
+end
+
+post '/sessions' do
+  email, password = params[:email], params[:password]
+  user = User.authenticate(email, password)
+  if user
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    flash[:errors] = ["The email or password are incorrect"]
+    erb :"sessions/new"
+  end
+
+end
+
+
 post '/users' do
   @user = User.create(:email => params[:email],
               :password => params[:password], 
