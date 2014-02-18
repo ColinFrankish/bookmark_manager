55d57afd7139d7de7472876637e2b0dac88d9bb4
Colin Frankish <colin.frankish@gmail.com>
2014-02-18 16:18:53 +0000

    links animate added

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,3 +2,10 @@ console.log("hello, makers academy!");
 $(function() {  // this is the shortcut to the $(document).ready(function()  -- lets the whole page load before running
 	console.log($("*").length); // this is counting the number of html elements in the document.
 });
+
+function animateLinks() {
+	$('.link').show(1500);
+}
+$(function() {
+	animateLinks();
+});
