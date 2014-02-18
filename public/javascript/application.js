37cc799640da43f8935839fc7622c3010d690fde
Colin Frankish <colin.frankish@gmail.com>
2014-02-18 18:18:04 +0000

    Favourites star added

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -9,3 +9,12 @@ function animateLinks() {
 $(function() {
 	animateLinks();
 });
+
+function addFavouritesHandler() {
+	$(".star.solid").click(function() {
+		$(this).animate({opacity: 1}, 1000);
+	});
+}
+$(function() {
+	addFavouritesHandler();
+})
