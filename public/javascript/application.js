0b4c716de72d58cd9ad95f3181219caefc4666be
Colin Frankish <colin.frankish@gmail.com>
2014-02-18 19:13:27 +0000

    Jquery used to store favourites

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -12,7 +12,10 @@ $(function() {
 
 function addFavouritesHandler() {
 	$(".star.solid").click(function(event) {
-		var newOpacity = 1 - parseInt($(this).css('opacity'));
+		var link = $(this).parent();
+		var favourited = !!$(link).data("favourited");
+		var newOpacity = favourited ? 0 : 1;
+		$(link).data("favourited", !favourited);
 		$(this).animate({opacity: newOpacity}, 1000);
 	});
 }
