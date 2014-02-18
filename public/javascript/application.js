5e6a2a46b583b586cc9f808cfb50471fe976da09
Colin Frankish <colin.frankish@gmail.com>
2014-02-18 18:43:14 +0000

    swishy star UI added

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -11,8 +11,9 @@ $(function() {
 });
 
 function addFavouritesHandler() {
-	$(".star.solid").click(function() {
-		$(this).animate({opacity: 1}, 1000);
+	$(".star.solid").click(function(event) {
+		var newOpacity = 1 - parseInt($(this).css('opacity'));
+		$(this).animate({opacity: newOpacity}, 1000);
 	});
 }
 $(function() {
