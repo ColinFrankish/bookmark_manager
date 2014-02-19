43fa6ad6fd906b53d36592b7e5612c489330a2b5
Colin Frankish <colin.frankish@gmail.com>
2014-02-19 17:43:42 +0000

    AJAX new forms handler added

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -25,10 +25,14 @@ function addFavouritesHandler() {
 }
 
 
-function prepareNewLinkHandler () {
-	$('.add-link').click(function(event) {
+function prepareNewFormsHandler () {
+	$('.add-link, .new-user, new-session').click(function(event) {
 		$.get($(this).attr("href"), function(data) {
-			console.log(data);
+			if ($('#container #ajax-form').length == 0) {
+        $("#container").prepend("<div id='ajax-form'></div>");
+      }
+      $('#container #ajax-form').html(data);
+
 		});
 		event.preventDefault();
 	});
@@ -37,6 +41,6 @@ function prepareNewLinkHandler () {
 
 $(function() {
 	addFavouritesHandler();
-	prepareNewLinkHandler();
+	prepareNewFormsHandler();
 })
 
